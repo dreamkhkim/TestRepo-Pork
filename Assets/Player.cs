6c9c63376fa4c068f27e9d4dd13da7082c565d93
coosdog <31675880+dreamkhkim@users.noreply.github.com>
2023-12-12 20:36:08 +0900

    플레이어 공격추가

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            Debug.Log("ATTack");
     }
 }
